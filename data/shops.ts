24424628a46e3d8a42ad08f1f6943b87318792b0
tatsuya-asami <sharekobekko@gmail.com>
2022-07-10 15:15:11 +0900

    Modify SHOPS params

--- data/shops.ts
+++ data/shops.ts
@@ -1,6 +1,12 @@
-export const SHOPS = [
-  { name: "俺の塩" },
-  { name: "君の豚" },
-  { name: "Your soy" },
-  { name: "MyMiso" },
+export type Shop = Readonly<{
+  name: string;
+  rank: 1 | 2 | 3 | 4 | 5;
+  bringIn: boolean;
+}>;
+
+export const SHOPS: Shop[] = [
+  { name: "俺の塩", rank: 3, bringIn: false },
+  { name: "君の豚", rank: 2, bringIn: true },
+  { name: "Your soy", rank: 4, bringIn: false },
+  { name: "MyMiso", rank: 5, bringIn: false },
 ];
